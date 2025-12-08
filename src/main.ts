789b631e4c4d4bb2e2910f036991fd3d7cf749ef
WilliansAS <anthowill1608@gmail.com>
2025-12-07 21:56:26 -0500

    Configurar CORS

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,16 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const configService = app.get(ConfigService);
+
+  app.enableCors();
 
   const port = configService.getOrThrow<number>('PORT');
 
+  await app.listen(port);
 
-  await app.listen(port); 
-  
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
